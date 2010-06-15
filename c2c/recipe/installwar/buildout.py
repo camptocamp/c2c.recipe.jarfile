44f475d9172af3f5b950f31788b5c33ce6ad7904
Frederic Junod <frederic.junod@gmail.com>
2010-06-15 12:02:28 +0200

    create or update and install the war

--- c2c/recipe/installwar/buildout.py
+++ c2c/recipe/installwar/buildout.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
 # buildout recipe for c2c.recipe.installwar
 #
+import os
+import shutil
+from subprocess import call, STDOUT
 
 class InstallWar(object):
     def __init__(self, buildout, name, options):
+        self.name = name
         basedir = buildout['buildout']['directory']
 
-    def install(self):
-        pass
+        self.input = [os.path.join(basedir, f) for f in options['input'].split()]
+        self.output = os.path.join(basedir, options['output'])
+        # check if os.basedir(self.output) exists and is writable
 
-    def update(self):
-        pass
+    def install(self):
+        warname = os.path.basename(self.output)
+        tmpwar = os.path.join(tempfile.mkdtemp(), warname)
+        errors = tempfile.TemporaryFile()
+        retcode = call("jar -cf %s %s"%(tmpwar, ' '.join(self.input)).split(), 
+                       cwd=dirname, stdout=errors, stderr=STDOUT)
+
+        if retcode != 0:
+            errors.seek(0)
+            # raise
+
+        shutil.copystat(tmpwar, self.output)
+        shutil.rmtree(os.path.dirname(tmpwar))
+
+        # returns the war and the directory name
+        return [self.output, self.output.replace('.war', '')]
+
     update = install
